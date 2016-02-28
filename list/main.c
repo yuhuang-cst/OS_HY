3f50d122a983578c0cccfa48a0434ff18fad34fc
HY <294100252@qq.com>
2016-02-28 21:56:55 +0800

    编译

--- list/main.c
+++ list/main.c
@@ -1,13 +1,21 @@
 #include "list.h"
-#include "stdio.h"
 int main()
 {
-	list_entry_t* list_p1 = new list_entry_t();
+	list_entry_t* list_p1 = (list_entry_t*)malloc(sizeof(list_entry_t));
+	list_p1->prev =  (list_entry_t*)malloc(sizeof(list_entry_t));
+	list_p1->next =  (list_entry_t*)malloc(sizeof(list_entry_t));
+
+	list_entry_t* list_p2 = (list_entry_t*)malloc(sizeof(list_entry_t));
+	list_p2->prev =  (list_entry_t*)malloc(sizeof(list_entry_t));
+	list_p2->next =  (list_entry_t*)malloc(sizeof(list_entry_t));
+
 	list_init(list_p1);
-	list_entry_t* list_p2 = new list_entry_t();
 	list_add(list_p1, list_p2);
 	if (list_empty(list_p1)){
-		printf("list is empty");
+		printf("list is empty\n");
+	}
+	else{
+		printf("list is not empty\n");
 	}
 	return 0;
 }
