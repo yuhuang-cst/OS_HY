b4d38ba9052c990d20805cc76aa713cc8b4b13c0
s0102 <yu-huang13@mails.tsinghua.edu.cn>
2016-03-09 19:16:46 +0800

    将memory数组改为char

--- 03-2-spoc/第三题/page_translate.cpp
+++ 03-2-spoc/第三题/page_translate.cpp
@@ -7,13 +7,13 @@ const int PAGE_NUM = 128;
 const int PAGE_SIZE = 32;
 const int PDBR = 544;
 
-int memory[PAGE_NUM * PAGE_SIZE];
+unsigned char memory[PAGE_NUM * PAGE_SIZE];
 
 void outputMem()
 {
 	for (int i = 0; i < PAGE_NUM; ++i){
 		for (int j = 0; j < PAGE_SIZE; ++j){
-			cout << memory[i * 128 + j] << " ";
+			cout << hex << (int)memory[i * 128 + j] << " ";
 		}
 		cout << endl;
 	}
@@ -25,11 +25,14 @@ int main()
 	if (!fin)
 		cout << "failed to open file" << endl;
 	string page_s, page_num_s;
+	int temp;
 	for (int i = 0; i < PAGE_NUM; ++i){
 		fin >> page_s >> page_num_s;
 		int base = i * PAGE_SIZE;
-		for (int j = 0; j < PAGE_SIZE; ++j)
-			fin >> hex >> memory[base + j];
+		for (int j = 0; j < PAGE_SIZE; ++j){
+			fin >> hex >> temp;
+			memory[base + j] = temp;
+		}
 	}
 
 	cout << "Virtual Address: ";
@@ -43,7 +46,7 @@ int main()
 	virtual_addr /= 32;
 	pde_index = virtual_addr % 32;
 
-	int pde = memory[PDBR + pde_index];
+	unsigned char pde = memory[PDBR + pde_index];
 	int pt_base = pde & 127;
 	if (pde / 128)
 		cout << "--> pde index:0x" << hex << pde_index << " pde contents:(valid " << 1 << ", pfn 0x" << hex << pt_base << ")" << endl;
@@ -52,7 +55,7 @@ int main()
 		return 0;
 	}
 
-	int pte = memory[PAGE_SIZE * pt_base + pte_index];
+	unsigned char pte = memory[PAGE_SIZE * pt_base + pte_index];
 	int physical_page = pte & 127;
 	if (pte / 128)
 		cout << "    " << "--> pte index:0x" << hex << pte_index << " pte contents:(valid " << 1 << ", pfn 0x" << hex << physical_page << ")" << endl;
@@ -62,7 +65,7 @@ int main()
 	}
 
 	int physical_addr = physical_page * 32 + offset;
-	cout << "        " << "--> Translates to Physical Address 0x" << hex << physical_addr << " --> Value: 0x" << hex << memory[physical_addr] << endl;
+	cout << "        " << "--> Translates to Physical Address 0x" << hex << physical_addr << " --> Value: 0x" << hex << (int)memory[physical_addr] << endl;
 
 
 	return 0;
